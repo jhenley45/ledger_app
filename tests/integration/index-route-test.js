b45155bdb758b71a8f13ee8972c1bf3524729da6
Jack Henley <jack.d.henley@gmail.com>
2015-08-07 08:57:59 -0400

    test passing for displaying title of trip in list on index route

--- tests/integration/index-route-test.js
+++ tests/integration/index-route-test.js
@@ -18,3 +18,18 @@ test('Should have a title of "All Trips"', function(assert) {
     assert.equal(find('h3').text(), 'All Trips');
   });
 });
+
+test('Should display a list of trips', function(assert) {
+  visit('/').then(function() {
+    assert.equal(find('li').length, 3);
+  });
+});
+
+test('Should display the title of each trip', function(assert) {
+  visit('/').then(function() {
+		var list = find('li');
+		$.each(list, function(i, v) {
+			assert.equal(v.innerHTML, "Trip " + (i + 1).toString());
+		});
+  });
+});
