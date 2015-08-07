4e936e5631a3924b6b9604c53bf5b2cf7271e8ba
Jack Henley <jack.d.henley@gmail.com>
2015-08-07 12:42:04 -0400

    added quick test for presence of submit button for new trip

--- ember/tests/integration/new-trip-test.js
+++ ember/tests/integration/new-trip-test.js
@@ -22,6 +22,10 @@ test('Should have an input for the Title field', function(assert) {
   assert.equal(find('input#trip-title').length, 1);
 });
 
+test('Should have a submit button', function(assert) {
+  assert.equal(find('button:contains("Submit")').length, 1);
+});
+
 test('Should show an error message when the user submits the form with an empty title', function(assert) {
   click(find('button:contains("Submit")')).then(function() {
     assert.equal(find('div.error-message:contains("Title cannot be empty")').length, 1);
