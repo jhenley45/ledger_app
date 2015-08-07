d2b0047077e29b87c43b1b0aa6445c05553b0b56
Jack Henley <jack.d.henley@gmail.com>
2015-08-07 11:13:00 -0400

    added upstart script for dev mode. dont use mocks in dev mode

--- server/index.js
+++ server/index.js
@@ -16,6 +16,15 @@ module.exports = function(app) {
   var morgan  = require('morgan');
   app.use(morgan('dev'));
 
+  // don't use mocks if env variable is set
+  function usingProxy() {
+      return !!process.argv.filter(function (arg) {
+          return arg.indexOf('--proxy') === 0;
+      }).length;
+  }
+
+  if (usingProxy()) { return; }
+
   mocks.forEach(function(route) { route(app); });
   proxies.forEach(function(route) { route(app); });
 
