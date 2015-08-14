25034d1a7855c0837085e84600cefc8233e784f4
Jack Henley <jack.d.henley@gmail.com>
2015-08-14 18:17:57 -0400

    added passing test for presence of new payment title

---
+++ ember/tests/integration/new-payment-test.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from '../helpers/start-app';
+
+var App;
+
+module('Integration - New Payment Page', {
+  beforeEach: function() {
+    App = startApp();
+    visit('/trips/2/payments/new');
+  },
+  afterEach: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('Should have a title of "New Payment"', function(assert) {
+  assert.equal(find('h4').text(), 'New Payment');
+});
+
+
+test('Should have a button to allow the user to create a new payment for the trip', function(assert) {
+  assert.equal(find('button.standard-button:contains("Add payment")').length, 1);
+});
+
+// test('Should display a form when the user clicks on the new payment button', function(assert) {
+//   click(find('button.standard-button:contains("Add a new payment")')).then(function() {
+//   	assert.equal(find('div.form-group').length, 2);
+//   });
+// });
+
+// test('Should change the text of the link once the user has clicked on it', function(assert) {
+//   click(find('button.standard-button:contains("Add a new payment")')).then(function() {
+//   	assert.equal(find('button.standard-button:contains("Cancel")').length, 1);
+//   });
+// });
