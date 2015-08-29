80086f37b5f67f1dbe6e1ece8661b4e390042fa7
Jack Henley <jack.d.henley@gmail.com>
2015-08-29 18:45:14 -0400

    all new payment tests passing

--- ember/tests/integration/new-payment-test.js
+++ ember/tests/integration/new-payment-test.js
@@ -8,7 +8,7 @@ module('Integration - New Payment', {
   beforeEach: function() {
     App = startApp();
     visit('/trips/2').then(function() {
-      click(find('button.standard-button:contains("Add a new payment")'));
+      click(find('div#new-payment-button'));
     });
   },
   afterEach: function() {
@@ -16,54 +16,50 @@ module('Integration - New Payment', {
   }
 });
 
-test('Should have a title of "New Payment"', function(assert) {
-  assert.equal(find('h4.new-payment').text(), 'New Payment');
-});
-
 
 test('Should have a button to allow the user to create a new payment for the trip', function(assert) {
-  assert.equal(find('button.standard-button:contains("Add payment")').length, 1);
+  assert.equal(find('div.standard-button:contains("Add payment")').length, 1);
 });
 
 test('Should display a form with input fields for the new payment', function(assert) {
-  assert.equal(find('div.form-group').length, 2);
+  assert.equal(find('.transparent-input').length, 2);
 });
 
 test('Should display an error message when no amount is provided.', function(assert) {
   fillIn(find('input#description'), 'Test Payment 4');
-  click(find('button:contains("Add payment")')).then(function() {
-    assert.equal(find('p.error-message:contains("Amount field cannot be empty")').length, 1);
+  click(find('div:contains("Add payment")')).then(function() {
+    assert.equal(find('div.error-message:contains("Amount field cannot be empty")').length, 1);
   });
 });
 
 test('Should display an error message when amount is not an integer.', function(assert) {
   fillIn(find('input#description'), 'Test Payment 4');
   fillIn(find('input#amount'), 'Monayyyy');
-  click(find('button:contains("Add payment")')).then(function() {
-    assert.equal(find('p.error-message:contains("Amount value must be a number")').length, 1);
+  click(find('div:contains("Add payment")')).then(function() {
+    assert.equal(find('div.error-message:contains("Amount value must be a number")').length, 1);
   });
 });
 
 test('Should display an error message when empty string is entered.', function(assert) {
   fillIn(find('input#description'), 'Test Payment 4');
   fillIn(find('input#amount'), '  ');
-  click(find('button:contains("Add payment")')).then(function() {
-    assert.equal(find('p.error-message:contains("Amount field cannot be empty")').length, 1);
+  click(find('div:contains("Add payment")')).then(function() {
+    assert.equal(find('div.error-message:contains("Amount field cannot be empty")').length, 1);
   });
 });
 
 test('Should add the payment to the list of existing trip payments after it is created.', function(assert) {
   fillIn(find('input#description'), 'Test Payment 4');
   fillIn(find('input#amount'), '230');
-  click(find('button:contains("Add payment")')).then(function() {
-    assert.equal(find('li.payment').length, 4);
+  click(find('div:contains("Add payment")')).then(function() {
+    assert.equal(find('div.payment').length, 4);
   });
 });
 
 test('Should hide the new payment form after payment has been added.', function(assert) {
   fillIn(find('input#description'), 'Test Payment 4');
   fillIn(find('input#amount'), '230');
-  click(find('button:contains("Add payment")')).then(function() {
-    assert.equal(find('button.standard-button:contains("Add a new payment")').length, 1);
+  click(find('div:contains("Add payment")')).then(function() {
+    assert.equal(find('div#new-payment-button').length, 1);
   });
 });
