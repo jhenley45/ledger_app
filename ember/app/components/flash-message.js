fa7cb06d8a1e4e8456437ee29cf5c5af7a5423b1
Jack Henley <jack.d.henley@gmail.com>
2015-10-22 19:19:50 -0700

    use inheritance for flash messages

--- ember/app/components/flash-message.js
+++ ember/app/components/flash-message.js
@@ -2,7 +2,6 @@ import Ember from "ember";
 
 export default Ember.FlashMessageComponent = Ember.Component.extend({
   classNameBindings: ['setClass'],
-  classNames: ['flash-message'],
 
   setClass: function() {
     return this.get('firstFlash').className;
