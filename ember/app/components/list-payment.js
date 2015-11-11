5b1a93c96484539504c503ec262a42879f7612f2
Jack Henley <jack.d.henley@gmail.com>
2015-11-10 23:21:24 -0800

    delete payment tests passing

--- ember/app/components/list-payment.js
+++ ember/app/components/list-payment.js
@@ -8,5 +8,14 @@ export default Ember.ListPaymentComponent = Ember.Component.extend({
 
   mouseLeave : function() {
     this.set('showEditDelete', false);
+  },
+  actions : {
+    toggleConfirmRemovePayment : function() {
+      this.set('confirmRemove', !this.get('confirmRemove'));
+    },
+    removePayment: function() {
+      this.sendAction("delete", this.get('payment').get('content'));
+    }
   }
+
 });
--- ember/tests/integration/delete-payment-test.js
+++ ember/tests/integration/delete-payment-test.js
@@ -20,30 +20,45 @@ module('Integration - Delete Payment', {
 
 // DELETE PAYMENT
 test('Should have a button to allow the user to delete a payment from the trip if it belongs to that user', function(assert) {
-  assert.equal(find('i#remove-payment').length, 1);
+  triggerEvent(".payment:contains('You dropped')", "mouseenter").then(function() {
+    assert.equal(find('i#remove-payment').length, 1);
+  })
 });
 
 test('Should confirm that the payment should be deleted when user clicks to delete payment', function(assert) {
-  click(find('i#remove-payment')).then(function() {
-    assert.equal(find('button:contains("Yes")').length, 1);
-  });
+  triggerEvent(".payment:contains('You dropped')", "mouseenter").then(function() {
+    click(find('i#remove-payment')).then(function() {
+      assert.equal(find('button:contains("Yes")').length, 1);
+    });
+  })
 });
 
 test('Should delete the payment when the user clicks to confirm the deletion', function(assert) {
-  click(find('i#remove-payment')).then(function() {
-    click(find('button:contains("Yes")')).then(function() {
-      assert.equal(find('div.payment').length, 2);
+  triggerEvent(".payment:contains('You dropped')", "mouseenter").then(function() {
+    click(find('i#remove-payment')).then(function() {
+      click(find('button:contains("Yes")')).then(function() {
+        assert.equal(find('div.payment').length, 2);
+      });
+    });
+  })
+});
+
+test('Should display a cancel delete button when user clicks to delete payment', function(assert) {
+  triggerEvent(".payment:contains('You dropped')", "mouseenter").then(function() {
+    click(find('i#remove-payment')).then(function() {
+      assert.equal(find('button:contains("No, cancel")').length, 1);
     });
   });
 });
 
-test('Should display a cancel delete button when user clicks to delete payment', function(assert) {
-  click(find('i#remove-payment')).then(function() {
-    assert.equal(find('button:contains("No, cancel")').length, 1);
-  });
-});
-
 test('Should hide the delete payment option when the user clicks to cancel the deletion', function(assert) {
+  triggerEvent(".payment:contains('You dropped')", "mouseenter").then(function() {
+    click(find('i#remove-payment')).then(function() {
+      click(find('button:contains("No, cancel")')).then(function() {
+        assert.equal(find('button:contains("No, cancel")').length, 0);
+      })
+    });
+  });
   click(find('i#remove-payment')).then(function() {
     click(find('button:contains("No, cancel")')).then(function() {
       assert.equal(find('button:contains("No, cancel")').length, 0);
